36542d2d6aac71185a73f26b4a88f22b56ab30b4
wjiang4318 <wjiang4318@gmail.com>
2026-07-21 16:15:45 -0400

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        empty_dict = {}
+        empty_list = []
+        for string in strs:
+            key = ''.join(sorted(string)) #act, #pots --> opst
+            if key not in empty_dict: # evaulate if sort anagram exist, if not create a key with empty list
+                empty_dict[key] = []
+            empty_dict[key].append(string) # if sort anagram exist, we add the string to it
+
+        for value in empty_dict.values():
+            empty_list.append(value)
+
+        return empty_list
